fe0190e1c493c704bc5ca9ec4cdb2c8a16d92c12
Ricky Bennett <norman.bennett525@gmail.com>
2021-03-02 04:13:39 -0800

    test

--- ElevenNote/ElevenNote.Services/NoteService.cs
+++ ElevenNote/ElevenNote.Services/NoteService.cs
@@ -56,5 +56,26 @@ namespace ElevenNote.Services
                 return query.ToArray();
             }
         }
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
     }
 }
